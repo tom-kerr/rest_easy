2ea8008a6aacaef42c3f119944d219fb284eaec6
reklaklislaw <thomkerr@gmail.com>
2014-04-27 00:15:38 -0400

    add missing import

--- rest_easy/core/query.py
+++ rest_easy/core/query.py
@@ -14,6 +14,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import print_function
 
+import re
 import sys
 import pprint
 from copy import copy, deepcopy
