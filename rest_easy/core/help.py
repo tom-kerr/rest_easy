ab313b6b8af85c1cf9f07752371a4b95e4023185
reklaklislaw <thomkerr@gmail.com>
2014-05-17 14:49:52 -0400

    improve help function -- refactor and ascii escape code formatting

--- rest_easy/core/help.py
+++ rest_easy/core/help.py
@@ -23,15 +23,38 @@ elif major == 3:
 
 import pprint
 from collections import OrderedDict
-from colorama import init as colorama_init
-from colorama import Fore, Back, Style
-colorama_init()
 
 from .parser import Parser
 from .alt import AlternateInterface
 from .parameter import Source, API, ResourceMethod, Property
 
 
+class FmtStr(object):
+    indent = '    '
+    reset = '\033[0m'
+    bold = '\033[1m'
+    italic = '\033[3m'
+    under = '\033[4m'
+
+    @staticmethod
+    def header(string):
+        return (FmtStr.bold + FmtStr.under +
+                string + FmtStr.reset)
+    
+    @staticmethod
+    def subheader(string):
+        return (FmtStr.indent + FmtStr.bold + FmtStr.under +
+                string + FmtStr.reset)
+
+    @staticmethod
+    def field(string, indent=0):
+        s = ''
+        for i in range(0, indent):
+            s += FmtStr.indent
+        return (s + FmtStr.italic +
+                str(string) + FmtStr.reset)
+        
+        
 class Helper(object):
     """ Prints object information.
 
@@ -54,160 +77,107 @@ class Helper(object):
             dpla('v2').searchIn.title.help()
         
     """
-    fore = {'red': Fore.RED, 'black': Fore.BLACK,
-            'green': Fore.GREEN, 'yellow': Fore.YELLOW,
-            'blue': Fore.BLUE, 'white': Fore.WHITE,
-            'cyan': Fore.CYAN, 'magenta': Fore.MAGENTA,
-            'reset': Fore.RESET}
-
-    back = {'red': Back.RED, 'black': Back.BLACK,
-            'green': Back.GREEN, 'yellow': Back.YELLOW,
-            'blue': Back.BLUE, 'white': Back.WHITE,
-            'cyan': Back.CYAN, 'magenta': Back.MAGENTA,
-            'reset': Back.RESET}
-
-    style = {'dim': Style.DIM, 'normal': Style.NORMAL ,
-             'bright': Style.BRIGHT, 'reset': Style.BRIGHT}
 
     def help(self, string=None):
         self._buffer_ = ''
         if string is None:
-            self._print_sources_()
+            self._get_sources_()
         else:
             source, api, detail = Parser._get_query_elements_(string)
             source_obj = self.get_wrappers(source)
             if not api:
-                self._print_source_details_(source_obj)
+                self._get_parameters_(source_obj)
             else:
                 api_obj = getattr(source_obj, api)
                 if not detail:
-                    self._print_api_specs_(api_obj)
+                    self._get_parameters_(api_obj)
                 else:
-                    self._print_detail_(api_obj, detail)
-        print (self._color_(self._buffer_, back='black'))
+                    self._get_detail_(api_obj, detail)
+        print (self._buffer_)
 
-    def _print_sources_(self):
-        self._buffer_ += self._color_('\nSource List\n', fore='red')
+    def _get_sources_(self):
+        self._buffer_ += ('\n' + FmtStr.header('Source List') + '\n')
         for source in self.source_list:
-            self._buffer_ += '\t' + self._color_(source, fore='green') + '\n'
+            self._buffer_ += (FmtStr.field(source) + '\n')
 
-    def _color_(self, string, **kwargs):
-        colored_string = ''
-        for atr in ('fore', 'back', 'style'):
-            if atr in kwargs and kwargs[atr]:
-                color = kwargs[atr]
-                colored_string += getattr(self, atr)[color]
-        colored_string += str(string)
-        for atr in ('fore', 'back', 'style'):
-            if atr in kwargs and kwargs[atr]:
-                colored_string += getattr(self, atr)['reset']
-        return colored_string
-
-    def _print_source_details_(self, source):
-        #if source.__doc__:
-        #    self._buffer_ += '\n\t' + self._color_(source.__doc__, fore='yellow') + '\n'
-        self._print_parameters_(source)
-
-    def _print_api_specs_(self, api):
-        #if api.__doc__:
-        #    self._buffer_ += '\n\t' + self._color_(api.__doc__, fore='yellow') + '\n'
-        self._print_parameters_(api)
-
-    def _print_essential_(self, obj):
+    def _get_essential_(self, obj):
         if hasattr(obj, '_essential_'):
             if obj._essential_:
-                self._buffer_ += ('    ' + self._color_('essential: ', fore='blue') +
-                                  self._color_(obj._essential_, fore='white') + '\n')
+                self._buffer_ += (FmtStr.subheader('essential:') +
+                                  FmtStr.field(obj._essential_, 2) + '\n')
 
-    def _print_requirements_(self, obj):
+    def _get_requirements_(self, obj):
         if hasattr(obj, '_requirements_'):
             requirements = obj._requirements_
             if requirements:
-                self._buffer_ += '\n    ' + self._color_('requirements: \n', fore='blue')
+                self._buffer_ += '\n' + FmtStr.subheader('requirements') + ': \n'
                 for k, v in requirements.required.items():
                     if v:
                         values = StringIO()
                         pprint.pprint(v, stream=values)
-                        self._buffer_ += '\t' + self._color_(k, fore='cyan')
+                        self._buffer_ += FmtStr.field(k, 2)
                         strings = values.getvalue().split('\n')
                         for s in strings:
-                            self._buffer_ += self._color_('\n\t    ' + s,
-                                                          fore='white')
-                        self._buffer_ += '\n'
+                            self._buffer_ += '\n' + FmtStr.field(s, 3)
 
-    def _print_parameters_(self, obj):
-        sources = []
-        apis = []
-        methods = []
-        properties = []
+    
+    def _get_parameters_(self, obj):
+        params = OrderedDict([('sources', []),
+                              ('apis', []),
+                              ('methods', []),
+                              ('properties', [])])
         obj_type = obj.__class__.__name__
         if obj_type == 'function':
             obj_type = 'Property'
 
-        self._buffer_ += ('\n' + self._color_(obj_type, fore='red') +
-                          self._color_(' "'+obj._name_+'"', fore='magenta') + '\n')
+        self._buffer_ += ('\n' + FmtStr.header(obj_type) +
+                          ' "'+obj._name_+'"' + '\n')
         if obj.__doc__:
             for s in obj.__doc__.split('\n'):
-                self._buffer_ += '    ' + self._color_(s, fore='yellow') + '\n'
+                self._buffer_ += '\n' + FmtStr.field(s, 1)
 
-        #consolidate?
         if hasattr(obj, '_http_method_'):
-            self._buffer_ += ('    ' + self._color_('http_method: ', fore='blue') +
-                              self._color_(obj._http_method_, fore='cyan') + '\n')
-        self._print_essential_(obj)
-        self._print_requirements_(obj)
+            self._buffer_ += ('\n' + FmtStr.subheader('http_method') + ': ' +
+                              FmtStr.field(obj._http_method_) + '\n')
+        self._get_essential_(obj)
+        self._get_requirements_(obj)
         if hasattr(obj, '__call__'):
-           self._print_callable_(obj)
+           self._get_callable_(obj)
 
         for attrname in dir(obj):
             if not self._is_internal_(attrname):
                 attr = getattr(obj, attrname)
-                if isinstance(attr, ResourceMethod):
-                    methods.append(attrname)
-                elif isinstance(attr, Property) or attr.__class__.__name__ == 'function':
-                    properties.append(attrname)
+                if isinstance(attr, Source):
+                    params['sources'].append(attrname)
                 elif isinstance(attr, API):
-                    apis.append(attrname)
-                elif isinstance(attr, Source):
-                    sources.append(attrname)
-
-        self._buffer_ += '\n'
-
-        if sources:
-            self._buffer_ += self._color_('    sources:', fore='blue')
-            for source in sources:
-                self._buffer_ += '\n\t' + self._color_(source, fore='green')
-            self._buffer_ += '\n'
-        if apis:
-            self._buffer_ += self._color_('    apis:', fore='blue')
-            for api in apis:
-                self._buffer_ += '\n\t' + self._color_(api, fore='green')
-            self._buffer_ += '\n'
-        if methods:
-            self._buffer_ += self._color_('    methods:', fore='blue')
-            for method in methods:
-                self._buffer_ += '\n\t' + self._color_(method, fore='green')
-            self._buffer_ += '\n'
-        if properties:
-            self._buffer_ += self._color_('    properties:', fore='blue')
-            for prop in properties:
-                self._buffer_ += '\n\t' + self._color_(prop, fore='green')
-            self._buffer_ += '\n'
-
-    def _print_callable_(self, obj):
+                    params['apis'].append(attrname)
+                elif isinstance(attr, ResourceMethod):
+                    params['methods'].append(attrname)
+                elif isinstance(attr, Property) or \
+                  attr.__class__.__name__ == 'function':
+                    params['properties'].append(attrname)
+        for param, fields in params.items():
+            if fields:
+                self._buffer_ += '\n' + FmtStr.subheader(param) + ':'
+                for f in fields:
+                    self._buffer_ += '\n' + FmtStr.field(f, 2)
+                self._buffer_ += '\n'
+                
+    def _get_callable_(self, obj):
         if not obj._key_ and not obj._expected_value_:
             return
-        self._buffer_ += (self._color_('    key: ', fore='blue') +
-                          self._color_(obj._key_, fore='cyan'))
-        if obj._expected_value_ and type(obj._expected_value_).__name__ == 'SRE_Pattern':
-            self._buffer_ += ('\n    ' + self._color_('expected_value: ', fore='blue') +
-                              self._color_(obj._expected_value_.pattern, fore='cyan'))
+        self._buffer_ += ('\n' + FmtStr.subheader('key') + ': ' +
+                          FmtStr.field(obj._key_))
+        if obj._expected_value_ and \
+          type(obj._expected_value_).__name__ == 'SRE_Pattern':
+            ev = str(obj._expected_value_.pattern)
         else:
-            self._buffer_ += ('\n    ' + self._color_('expected_value: ', fore='blue') +
-                              self._color_(obj._expected_value_, fore='cyan'))
-        self._buffer_ += '\n'
-
-    def _print_detail_(self, api, detail):
+            ev = str(obj._expected_value_)
+          
+        self._buffer_ += ('\n' + FmtStr.subheader('expected_value') + ': '
+                            + FmtStr.field(ev) + '\n')
+      
+    def _get_detail_(self, api, detail):
         elements = Parser._parse_query_string_(detail, mode='lookup')
         self._find_detail_(api, [elements,])
 
@@ -215,7 +185,7 @@ class Helper(object):
         for elem in elements:
             if isinstance(elem, str):
                 attr = getattr(root, elem)
-                self._print_parameters_(attr)
+                self._get_parameters_(attr)
 
             elif isinstance(elem, dict):
                 for k, v in elem.items():
@@ -226,7 +196,7 @@ class Helper(object):
                         return
                     elif v and hasattr(root, v):
                         root = getattr(root, v)
-                    self._print_parameters_(root)
+                    self._get_parameters_(root)
 
     def _is_internal_(self, attr):
         if attr.startswith('_') and attr.endswith('_'):
