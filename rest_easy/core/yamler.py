22203dcb258519b6079567aa841c1edabb2ae41a
reklaklislaw <thomkerr@gmail.com>
2014-05-18 13:07:45 -0400

    add yaml import support for nested source documents

--- rest_easy/core/yamler.py
+++ rest_easy/core/yamler.py
@@ -53,6 +53,33 @@ class Yamler(object):
         total = []
         lines = self.get_lines(source_file)
         _lines = copy(lines)
+        imports = self.walk_imports(lines, total, _lines)
+        docs = []
+        if '---\n' in _lines:
+            while '---\n' in _lines:
+                i = _lines.index('---\n')
+                docs.append(_lines[:i])
+                _lines = _lines[i+1:]
+            docs.append(_lines)
+        else:
+            docs.append(_lines)
+
+        for num, doc in enumerate(docs):
+            if imports:
+                total.extend(imports + doc)
+            else:
+                total.extend(doc)
+            if len(docs) > 1 and num < len(docs)-1:
+                total.append('---\n')
+              
+        if prevbuf:
+            return total
+        else:
+            total = StringIO(''.join(total))
+            return yaml.load_all(total)
+
+    def walk_imports(self, lines, total, _lines):
+        imports = []
         for num, line in enumerate(lines):
             if line == '\n' or line.startswith('#'):
                 continue
@@ -65,20 +92,10 @@ class Yamler(object):
                 if basename not in self.imported:
                     self.imported.append(basename)
                     import_file = self.path + '/' + basename + '.yaml'
-                    results = self.load(import_file, True)
-                    if results:
-                        results = [basename + ': \n'] + results
-                        total.extend(results)
-        if prevbuf:
-            if total:
-                total.extend(_lines)
-                return total
-            else:
-                return _lines
-        else:
-            total.extend(_lines)
-            total = StringIO(''.join(total))
-            return yaml.load_all(total)
+                    i = self.load(import_file, True)
+                    if i:
+                        imports.extend([basename + ': \n'] + i)
+        return imports
 
     def get_lines(self, source_file):
         with open(source_file, encoding='utf-8', mode='r') as yamlfile:
